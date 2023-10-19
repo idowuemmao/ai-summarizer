fb07d5fdd6d90aaa72a40cc5494cb44f6d4ccd86
Idowu Emmanuel <idowuemmao@gmail.com>
2023-10-19 13:56:27 +0100

    background color changed

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Hero from "./components/Hero";
 
 const App = () => {
   return (
-    <main className="p-8">
+    <main className="p-8 bg-orange-100">
       <Hero />
       <Demo />
     </main>
