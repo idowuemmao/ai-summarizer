f6a6d79fdfbde7fab1f73fdba4552700867bc569
Idowu Emmanuel <idowuemmao@gmail.com>
2023-10-19 14:09:58 +0100

    corrected the background css property

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Hero from "./components/Hero";
 
 const App = () => {
   return (
-    <main className="p-8 bg-orange-100">
+    <main className="p-10 bg-orange-100 h-screen">
       <Hero />
       <Demo />
     </main>
